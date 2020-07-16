2193a57f121cfd538d13a1b3e4a8dad5fc51847c
Yisheng Li <yishengli8686@gmail.com>
2020-07-15 22:49:36 -0400

    Tidy up

--- Filter/GalleryViewController.swift
+++ Filter/GalleryViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 import CoreData
 
 
-
 class GalleryViewController: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource {
     
     
@@ -53,22 +52,5 @@ class GalleryViewController: UIViewController,UICollectionViewDelegate, UICollec
         print("You tapped me")
     }
     
-    
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return gallery.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let calendar = Calendar.current
-//        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
-//        cell.textLabel?.text = "\(String(describing: gallery[indexPath.row].filter)) + \(gallery[indexPath.row].volume)"
-//        cell.detailTextLabel?.text = String(calendar.component(.minute, from: gallery[indexPath.row].date!))
-//        return cell
-//    }
     
 }
